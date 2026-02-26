76100113c38be9eb6b3f25c80fc52701d057fe5f
Isaac Emmanuel <Gorillabackdev>
2026-02-26 14:45:13 -0800

    feat(users): add user management endpoints with admin role controls

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const authRoutes = require("./routes/auth.routes");
+const userRoutes = require("./routes/user.routes");
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(helmet());
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "Digital Wallet API Running" });
