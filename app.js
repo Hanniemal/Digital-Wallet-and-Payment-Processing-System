ba0590997e013f8d0c40d81bc7343f161a24e038
Isaac Emmanuel <Gorillabackdev>
2026-02-26 14:36:44 -0800

    feat(auth): implement registration, login, jwt auth, and role middleware

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
+const authRoutes = require("./routes/auth.routes");
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(helmet());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "Digital Wallet API Running" });
