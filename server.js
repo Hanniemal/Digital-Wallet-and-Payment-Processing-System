82aa27a563f95dd97eb45b280dfed195889ad8a0
Isaac Emmanuel <Gorillabackdev>
2026-02-27 04:21:59 -0800

    refactor: consolidate backend into src with root server entry

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
-const app = require("./app");
-const { initDb } = require("./config/db");
-const logger = require("./utils/logger");
+const app = require("./src/app");
+const { initDb } = require("./src/config/db");
+const logger = require("./src/utils/logger");
 
 const PORT = process.env.PORT || 5000;
 
